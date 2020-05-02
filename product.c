094fe2326d73a6f068ff5a61354844cff614ac05
chinmaishivaru <63471578+chinmaishivaru@users.noreply.github.com>
2020-05-02 23:13:43 +0530

    To calculate the product of two numbers.

---
+++ product.c
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+	using std::cout;
+	using std::cin;
+	float a,b;
+	cout<<"Enter two numbers:\t";
+	cin>>a>>b;
+	cout<<"\nProduct = "<<a*b;
+	return 0;
+}
